8394880f853b0f00c3fff458c795c8481432285f
Amir Bashiri <bashiri.net@gmail.com>
2021-06-27 13:08:01 +0430

    add inventory_builder.sh

---
+++ inventory_builder.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+## Change root path
+cd "$(dirname "$0")/" || exit
+PATH_ROOT="$(pwd)"
+
+cd kubespray
+
+if [ -d "${PATH_ROOT}/inventory" ]; then
+    echo "Inventory exists!"
+    exit 1
+fi
+
+cp -rfp inventory/sample "${PATH_ROOT}/inventory"
+export CONFIG_FILE="${PATH_ROOT}/inventory/inventory.yaml"
+
+source venv/bin/activate
+exec python contrib/inventory_builder/inventory.py $*
